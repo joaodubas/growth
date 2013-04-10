a9c07830554e844846a79054c2614f1a47c1c236
João Paulo Dubas <joao.dubas@gmail.com>
2013-04-10 20:56:44 -0300

    Testing implementation of helper library functions.

---
+++ tests/spec/age.js
@@ -0,0 +1,23 @@
+var age = require('../../lib/age');
+
+describe('Age manipulation', function () {
+    var doa = new Date(2013, 11, 15),
+        dob = new Date(1978, 11, 15);
+
+    function to_fixed(value, decimal) {
+        decimal = !isNaN(decimal) ? decimal : 4;
+        return Number(Number.prototype.toFixed.call(value, decimal));
+    }
+
+    it('Should get proper age in days', function () {
+        expect(to_fixed(age.indays(doa, dob), 0)).toEqual(12784);
+    });
+
+    it('Should get proper age in months', function () {
+        expect(to_fixed(age.inmonths(doa, dob), 0)).toEqual(420);
+    });
+
+    it('Should get proper age in years', function () {
+        expect(to_fixed(age.inyears(doa, dob), 0)).toEqual(35);
+    });
+});
---
+++ tests/spec/statistic.js
@@ -0,0 +1,30 @@
+var snd = require('../../lib/statistic');
+
+describe('Calculations over the standard normal distribution', function () {
+    function to_fixed(value, decimal) {
+        decimal = !isNaN(decimal) ? decimal : 4;
+        return Number(Number.prototype.toFixed.call(value, decimal));
+    }
+
+    var zscores = [-3, -2, -1, 0, 1, 2, 3],
+        unipvalues = [0.0013, 0.0228, 0.1587, 0.5000, 0.8413, 0.9772, 0.9987],
+        bipvalues = [0.0044, 0.0540, 0.2420, 0.3989, 0.2420, 0.0540, 0.0044];
+
+    it('Should calculate proper pdf function', function () {
+        zscores.forEach(function (z, index) {
+            expect(to_fixed(snd.pdf(z))).toEqual(bipvalues[index]);
+        });
+    });
+
+    it('Should calculate proper cdf function', function () {
+        zscores.forEach(function (z, index) {
+            expect(to_fixed(snd.cdf(z))).toEqual(unipvalues[index]);
+        });
+    });
+
+    it('Should calculate proper inverse for cdf function', function () {
+        unipvalues.forEach(function (p, index) {
+            expect(to_fixed(snd.icdf(p), 0)).toEqual(zscores[index]);
+        });
+    });
+});
