472cb6e7734433bd712212da0f7a922ec4910382
João Paulo Dubas <joao.dubas@gmail.com>
2013-04-18 06:06:06 -0300

    Test data access interface and improve test of streams.

--- test/lms.js
+++ test/lms.js
@@ -1,18 +1,15 @@
 var expect = require('expect.js');
 var mockstream = require('./helper/stream.js');
+var to_fixed = require('./helper/math.js').to_fixed;
 
 var calc = require('../lib/lms/calc.js');
 var parser = require('../lib/lms/stream.js');
 var transformer = require('../lib/lms/adapter.js');
 var constants = require('../lib/lms/constants.js');
+var LMS = require('../lib/lms/data.js');
 
 describe('BoxCox functionality', function () {
   describe('Conversions between z-score and measure', function () {
-    function to_fixed(value, decimal) {
-      decimal = !isNaN(decimal) ? decimal : 4;
-      return Number(Number.prototype.toFixed.call(value, decimal));
-    }
-
     describe('From measure to z-score', function () {
       it('Get a z-score from measure and lms details', function () {
         var options = {
@@ -76,26 +73,14 @@ describe('BoxCox functionality', function () {
 
   describe('Data processing', function () {
     var data = [
-      {
-        age: 91,
-        l: 0.3933,
-        m: 13.4779,
-        s: 0.07474,
-        p01: 10.579
-      },
-      {
-        age: 92,
-        l: 0.3916,
-        m: 13.49,
-        s: 0.07476,
-        p01: 10.589
-      }
+      {age: 91, l: 0.3933, m: 13.4779, s: 0.07474, p01: 10.579},
+      {age: 92, l: 0.3916, m: 13.49, s: 0.07476, p01: 10.589}
     ];
-    var datameta = {
+    var meta = {
       key: 'age',
       measure: constants.CIRCUNFERENCE,
       by: constants.AGEMONTH
-    }
+    };
 
     function toSource(data) {
       var source = data.map(function (hash) {
@@ -172,12 +157,19 @@ describe('BoxCox functionality', function () {
       var interceptor = null;
       var converter = null;
 
+      function execution(callback) {
+        interceptor.on('finish', function () {
+          callback(interceptor._accum[0]);
+        });
+        reader.pipe(converter).pipe(interceptor);
+      }
+
       beforeEach(function () {
         reader = new mockstream.MockReader({
           source: data.map(JSON.stringify)
         });
         interceptor = new mockstream.MockTransformer();
-        converter = new transformer.MemoryStream(datameta);
+        converter = new transformer.MemoryStream(meta);
       });
 
       afterEach(function () {
@@ -187,91 +179,135 @@ describe('BoxCox functionality', function () {
       });
 
       it('Add meta information in `meta` property', function (done) {
-        interceptor.on('finish', function () {
-          expect(JSON.parse(interceptor._accum[0]).meta).to.eql({
-            measure: datameta.measure,
-            by: datameta.by
+        execution(function (data) {
+          expect(JSON.parse(data).meta).to.eql({
+            measure: meta.measure,
+            by: meta.by
           });
           done();
         });
-
-        reader.pipe(converter).pipe(interceptor);
       });
 
       it('Create dict of age keys with lms values', function (done) {
-        interceptor.on('finish', function () {
-          expect(JSON.parse(interceptor._accum[0]).xcoord).to.eql({
-            91: {
-              l: 0.3933,
-              m: 13.4779,
-              s: 0.07474
-            },
-            92: {
-              l: 0.3916,
-              m: 13.49,
-              s: 0.07476
-            }
+        execution(function (data) {
+          expect(JSON.parse(data).xcoord).to.eql({
+            91: {l: 0.3933, m: 13.4779, s: 0.07474},
+            92: {l: 0.3916, m: 13.49, s: 0.07476}
           });
           done();
         });
-
-        reader.pipe(converter).pipe(interceptor);
       });
 
       it('Create list of percentile values', function (done) {
-        interceptor.on('finish', function () {
-          expect(JSON.parse(interceptor._accum[0]).percentile).to.eql([
-            {
-              xcoord: 91,
-              p01: 10.579
-            },
-            {
-              xcoord: 92,
-              p01: 10.589
-            }
+        execution(function (data) {
+          expect(JSON.parse(data).percentile).to.eql([
+            {xcoord: 91, p01: 10.579},
+            {xcoord: 92, p01: 10.589}
           ]);
-
           done();
         });
-        reader.pipe(converter).pipe(interceptor);
       });
 
       it('Convert data into proper format', function (done) {
-        var expectation = {
-          meta: {
-            measure: datameta.measure,
-            by: datameta.by
-          },
-          xcoord: {
-            91: {
-              l: 0.3933,
-              m: 13.4779,
-              s: 0.07474
+        execution(function (data) {
+          var expectation = {
+            meta: {
+              measure: meta.measure,
+              by: meta.by
             },
-            92: {
-              l: 0.3916,
-              m: 13.49,
-              s: 0.07476
-            }
-          },
-          percentile: [
-            {
-              xcoord: 91,
-              p01: 10.579
+            xcoord: {
+              91: {l: 0.3933, m: 13.4779, s: 0.07474},
+              92: {l: 0.3916, m: 13.49, s: 0.07476}
             },
-            {
-              xcoord: 92,
-              p01: 10.589
-            }
-          ]
-        }
-
-        interceptor.on('finish', function () {
-          expect(interceptor._accum[0]).to.equal(JSON.stringify(expectation));
+            percentile: [
+              {xcoord: 91, p01: 10.579},
+              {xcoord: 92, p01: 10.589}
+            ]
+          };
+          expect(data).to.equal(JSON.stringify(expectation));
           done();
         });
+      });
+    });
 
-        reader.pipe(converter).pipe(interceptor);
+    describe('Data access', function () {
+      var reader = null;
+      var interceptor = null;
+      var lmsparser = null;
+      var converter = null;
+
+      function execution(callback) {
+        interceptor.on('finish', function () {
+          callback(interceptor._accum[0]);
+        });
+        reader.pipe(lmsparser).pipe(converter).pipe(interceptor);
+      }
+
+      beforeEach(function () {
+        reader = new mockstream.MockReader({
+          source: toSource(data)
+        });
+        interceptor = new mockstream.MockTransformer();
+        lmsparser = new parser.LMSStream();
+        converter = new transformer.MemoryStream(meta);
+      });
+
+      afterEach(function () {
+        reader = null;
+        interceptor = null;
+        lmsparser = null;
+        converter = null;
+      });
+
+      var age = 91;
+      var measure = 13.4779;
+      var percentile = 0.5;
+      var zscore = 0;
+
+      it('Get lms parameters for age and measure', function (done) {
+        execution(function (data) {
+          var lms = {
+            power: 0.3933,
+            median: 13.4779,
+            cv: 0.07474
+          };
+          var db = new LMS(JSON.parse(data));
+          expect(db._params(age)).to.eql(lms);
+          done();
+        });
+      });
+      it('Get zscore for age and measure', function (done) {
+        execution(function (data) {
+          var db = new LMS(JSON.parse(data));
+          expect(
+            to_fixed(db.zscore({xvalue: age, measure: measure}), 4)
+          ).to.eql(
+            to_fixed(zscore, 4)
+          );
+          done();
+        });
+      });
+      it('Get percentile for age and measure', function (done) {
+        execution(function (data) {
+          var db = new LMS(JSON.parse(data));
+          expect(
+            to_fixed(db.percentile({xvalue: age, measure: measure}), 4)
+          ).to.eql(
+            to_fixed(percentile, 4)
+          );
+          done();
+        });
+      });
+      it('Get centile for age and zcore', function (done) {
+        execution(function (data) {
+          var db = new LMS(JSON.parse(data));
+          expect(
+            to_fixed(db.centile({xvalue: age, zscore: zscore}), 4)
+          ).to.eql(
+            to_fixed(measure, 4)
+          );
+          done();
+        });
       });
     });
   });
